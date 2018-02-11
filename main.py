ae6782cb33dfaf02437c0eb2398d2c9b6b30caf3
Ringtail The Raccoon <cubepandamusic@gmail.com>
2018-02-10 20:06:26 -0500

    added some new stuff
    added in a few commands such as the github command and pong command. i also made the help command load the message from a file instead of using one long hard coded string.

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 import discord
 from discord.ext import commands
-from discord.ext.commands import Bot
-import asyncio
 import datetime
 import pytz
 import random
@@ -12,17 +10,22 @@ bot = commands.Bot(command_prefix='/')
 current_time = datetime.datetime.now(tz=pytz.UTC)
 line_sep = '--------------------'
 
-#sends message to console indicating that the bot has booted with out any errors
+#sends message to console indicating that the bot has booted with out any errors and also initializes some things
 @bot.event
 async def on_ready():
     print('Bot booted up at ' + str(current_time) + '\n' + line_sep)
+    bot.remove_command('help')
+    await bot.change_presence(game=discord.Game(name='Type "/helpme" for a list of commands'))
 
 #main info commands
 
 #help command
 @bot.command(pass_context=True)
 async def helpme(ctx):
-    await bot.say('**List of commands:**\n**/ping:** How to annoy a bot 101\n**/userinfor:** Gets info about a user\n**/serverinfo:** Gets info about the server you are currently in\n**/pfp:** Gets a users profile picture\n**/nuzzle:** Nuzzles you >w<\n**/groovycat:** Thats one hella groovy cat')
+    #have the string for this command load from a file cause this is way too long
+    f = open('helpme.txt', 'r')
+    await bot.say(f.read())
+    f.close()
     print('executed help command' + '\n' + line_sep)
 
 #ping command (work on getting the actual ping of bot connection)
@@ -31,7 +34,7 @@ async def ping(ctx):
     await bot.say('dont ping me motherfucker')
     print('executed ping command' + '\n' + line_sep)
 
-#user info command: gets data about user
+#user info command: gets data about a user
 @bot.command(pass_context=True)
 async def userinfo(ctx, user: discord.Member):
     embed = discord.Embed(title='User info for {}'.format(user.name) + '#{}'.format(user.discriminator), description='Also known as: {}'.format(user.display_name))
@@ -49,6 +52,7 @@ async def info_error(error, ctx):
     await bot.say('Please @ a user to view info about them!')
     print('returned error for info command (user arg not specified)' + '\n' + line_sep)
 
+#server info command: gets data about the server the command is run in 
 @bot.command(pass_context=True)
 async def serverinfo(ctx):
     embed = discord.Embed(title=ctx.message.server.name, description='Server region: {}'.format(ctx.message.server.region))
@@ -81,9 +85,14 @@ async def pfp_error(error, ctx):
     await bot.say('Please @ a user to view their pfp!')
     print('returned error for pfp command (user arg not specified)' + '\n' + line_sep)
 
+#github command: sends github page for TrashBot
+@bot.command(pass_context=True)
+async def github(ctx):
+    await bot.say('TrashBot Github page: https://github.com/RadicalRingtail/TrashBot')
+
 #fun commands
 
-#nuzzle command: pings the user who uses the command and nuzzles them >w<
+#nuzzle command: nuzzles you >w<
 @bot.command(pass_context=True)
 async def nuzzle(ctx):
     await bot.say('*nuzzles chu~* >w<')
@@ -92,21 +101,34 @@ async def nuzzle(ctx):
 @nuzzle.error
 async def nuzzle_error(error, ctx):
     await bot.say('something went wrong when i tried to nuzzle you, oof')
-    print('i literally just added this error catch just to be safe but if you actually somehow got this to show up in the console, i have no idea how you fucked this up')
+    print('i literally just added this error catch just to be safe but if you actually somehow got this to show up in the console, i have no idea how you fucked this up' + '\n' + line_sep)
 
 #groovy cat command: posts a groovy cat
 @bot.command(pass_context=True)
 async def groovycat(ctx):
     await bot.send_file(ctx.message.channel, 'images/gifs/groovycat.gif')
     print('executed groovy cat command' + '\n' + line_sep)
-    
+
+@groovycat.error
+async def groovycat_error(error, ctx):
+    await bot.say('the groovy cat is no where to be seen.')
+    print('groovy cat command returned a error: image could not be found' + '\n' + line_sep)
+
 #cursed image command: posts a random cursed image
 @bot.command(pass_context=True)
 async def cursedimage(ctx):
-    await bot.send_file(ctx.message.channel, 'images/cursed images/' + random.choice(os.listdir('...\images\cursed images')))
+    await bot.send_file(ctx.message.channel, 'images/cursed images/' + random.choice(os.listdir('TrashBot\images\cursed images')))
     print('executed cursed image command' + '\n' + line_sep)
 
+@cursedimage.error
+async def groovycat_error(error, ctx):
+    await bot.say('huh.. odd.. i could have sworn i had some cursed images here a second ago..')
+    print('cursed image command returned a error: images or directory could not be found' + '\n' + line_sep)
 
+#pong command
+@bot.command(pass_context=True)
+async def pong(ctx):
+    await bot.say(':ping_pong:')
 
 #search commands & web scraping
 
